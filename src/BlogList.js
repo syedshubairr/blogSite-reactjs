82acbf60c19f508e7c56db67f583d1427c8b0cca
syedshubairr <syedshubair8299@gmail.com>
2022-11-26 22:07:12 +0500

    Lecture 19

---
+++ src/BlogList.js
@@ -0,0 +1,18 @@
+const BlogList = ( {blogs, title} ) => {
+  
+  return (
+    <div className="blog-list">
+      <h1>{title}</h1>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>{blog.body}</p>
+          <p>Written by: {blog.author}</p>
+          <br />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogList;

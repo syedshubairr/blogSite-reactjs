7a1300f03bc7bfd988715e204e328b7893d6002d
syedshubairr <syedshubair8299@gmail.com>
2022-11-27 18:10:04 +0500

    Cleanup of useEffect using abort controller

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,25 +6,33 @@ function useFetch(url) {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch(url)
-      .then((res) => {
-        if (!res.ok) {
-          throw Error("Fetch Denied");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setError(null);
-        setData(data);
-        setPending(false);
-      })
-      .catch((err) => {
-        setError(err.message);
-        setPending(false);
-      });
+    const abortCon = new AbortController();
+    setTimeout(() => {
+      fetch(url, { signal: abortCon.signal })
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Fetch Denied");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setError(null);
+          setData(data);
+          setPending(false);
+        })
+        .catch((err) => {
+          if (err.name == "AbortError") {
+            console.log("Fetch Aborted");
+          } else {
+            setError(err.message);
+            setPending(false);
+          }
+        });
+    }, 500);
+    return () => abortCon.abort();
   }, [url]);
 
-  return {data, isPending, error};
+  return { data, isPending, error };
 }
 
 export default useFetch;
